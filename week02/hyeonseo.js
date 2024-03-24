a4d55318d053b74ae4261046d36f0d134e6c0087
kittyismylife <chss0520@sookmyung.ac.kr>
2024-03-24 16:15:08 +0900

    [Feat] 시간표 light/dark 모드 기능 추가

--- week02/hyeonseo.js
+++ week02/hyeonseo.js
@@ -3,12 +3,23 @@ confirm("정말 현서의 자기소개 페이지에 접속하시겠습니까?");
 prompt("접속자의 이름을 현서에게 알려주세요!", "");
 
 const mainbox = document.getElementById("mainbox");
-function darkmode() {
+function maindarkmode() {
     mainbox.style.backgroundColor = '#262626';
     mainbox.style.color = 'white';
 }
-function lightmode() {
+function mainlightmode() {
     mainbox.style.backgroundColor = 'white';
     mainbox.style.color = 'black';
+    h2.style.backgroundColor = 'cornsilk'
+}
+
+const schebox = document.getElementById("schebox");
+function schedarkmode() {
+    schebox.style.backgroundColor = '#262626';
+    schebox.style.color = 'white';
+}
+function schelightmode() {
+    schebox.style.backgroundColor = 'white';
+    schebox.style.color = 'black';
     h2.style.backgroundColor = 'cornsilk'
 }
