2b987f6b1952b59f9927bfbd171bd3674361a596
kittyismylife <chss0520@sookmyung.ac.kr>
2024-03-24 15:46:06 +0900

    [Feat] comfirm 및 prompt 기능 추가

--- week02/hyeonseo.js
+++ week02/hyeonseo.js
@@ -1,6 +1,6 @@
 alert("Welcome to hyeonseo info page!");
-confirm("Are you sure?");
-prompt("What your name?", "");
+confirm("정말 현서의 자기소개 페이지에 접속하시겠습니까?");
+prompt("접속자의 이름을 현서에게 알려주세요!", "");
 
 const mainbox = document.getElementById("mainbox");
 function darkmode() {
