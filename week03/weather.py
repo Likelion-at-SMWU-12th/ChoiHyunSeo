df6e8fa32b5c18f0891cd050977952c013ae7181
kittyismylife <chss0520@sookmyung.ac.kr>
2024-03-28 12:09:00 +0900

    [Feat] 검색기 실행 여부 판단

--- week03/weather.py
+++ week03/weather.py
@@ -9,3 +9,22 @@ def get_weather_info(city):
     result = requests.get(api)
     # 응답이 성공적으로 받아졌을 경우 (status_code가 200일 경우) JSON 형식의 응답을 파싱하여 반환
     return json.loads(result.text) if result.status_code == 200 else None
+
+while True:
+    print("======= 안녕하세요!~ 날씨 검색기입니다. =======")
+    print('날씨 검색기를 실행시키겠습니까?')
+    print('  y : 진행       n : 취소')
+    answer = input('>>> ')
+    answer = answer.lower()
+
+    if answer == 'y' or answer == "진행":
+        print("======================================================================")
+        print('날씨 검색기 프로그램이 실행됩니다.')
+        break
+    elif answer == 'n' or answer == "취소":
+        print("======================================================================")
+        print('날씨 검색기 프로그램이 종료됩니다.')
+        print("======================================================================")
+        exit()
+    else:
+        print("'y', '진행' 또는 'n', '취소'을 입력해주세요.")
