48c2ef15867d799ad12258201d050e4d66249d05
kittyismylife <chss0520@sookmyung.ac.kr>
2024-05-21 19:37:50 +0900

    Feat: PaintToDo 함수 작성

--- FRONT/Front_Week2_Practice/todo.js
+++ FRONT/Front_Week2_Practice/todo.js
@@ -3,7 +3,16 @@ const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
 function paintToDo(newTodo) {
-
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span,innerText - newTodo;
+    const button = document.createElement("button");
+    button.innerText = "😻";
+    button.addEventListener("click", deleteToDo);
+    li.appendChlid(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
