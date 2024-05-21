fe1623918831f3f00b64fdd3b5e22b3d35b9e032
kittyismylife <chss0520@sookmyung.ac.kr>
2024-05-21 19:33:19 +0900

    Create todo.js

---
+++ FRONT/Front_Week2_Practice/todo.js
@@ -0,0 +1,7 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+function paintToDo(newTodo) {
+    
+}
