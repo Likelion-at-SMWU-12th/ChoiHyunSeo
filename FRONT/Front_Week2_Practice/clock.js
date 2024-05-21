c0c80f17bdb15dbcefa59fa28e2d7411f43d91cc
kittyismylife <chss0520@sookmyung.ac.kr>
2024-05-21 19:33:33 +0900

    Fix: 시분초 2자리 차지하게 수정

--- FRONT/Front_Week2_Practice/clock.js
+++ FRONT/Front_Week2_Practice/clock.js
@@ -3,8 +3,8 @@ const clock = document.querySelector("h2#clock");
 function getClock() {
     const date = new Date();
     const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
     // console.log(`${hours}:${minutes}:${seconds}`)
     clock.innerHTML = `${hours}:${minutes}:${seconds}`;
     // console.log("getClock() 호출")
