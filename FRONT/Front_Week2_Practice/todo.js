6440f4c3ac2484c595f2ede584cc7312c88e5d69
kittyismylife <chss0520@sookmyung.ac.kr>
2024-05-21 19:35:43 +0900

    Feat: to-do list - (2) todo.js

--- FRONT/Front_Week2_Practice/todo.js
+++ FRONT/Front_Week2_Practice/todo.js
@@ -3,5 +3,12 @@ const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
 function paintToDo(newTodo) {
-    
+
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newTodo = toDoInput.ariaValueMax;
+    toDoInput.value = "";
+    paintToDo(newTodo)
 }
