d8d6a5268d2b2d6661dc87b5f1b39c62c754b641
kittyismylife <chss0520@sookmyung.ac.kr>
2024-05-27 10:52:21 +0900

    Fix: clock.js 수정 파일 바뀜 (실습 - 과제)

--- FRONT/Front_Week2_Practice/clock.js
+++ FRONT/Front_Week2_Practice/clock.js
@@ -1,25 +1,13 @@
-const clock = document.querySelector("h3#clock");
-const date_show = document.querySelector("h3#date");
+const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    // 연도, 월, 일
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-
-    // 요일 반영해주기
-    const dayIndex = date.getDay();
-    const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const dayName = dayNames[dayIndex];
-
-    // 시, 분, 초
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
-
-    date_show.innerHTML = `🗓️${year}-${month}-${day} (${dayName})`;
-    clock.innerHTML = `⏰ ${hours}:${minutes}:${seconds}`;
+    // console.log(`${hours}:${minutes}:${seconds}`)
+    clock.innerHTML = `${hours}:${minutes}:${seconds}`;
+    // console.log("getClock() 호출")
 }
 
 getClock();
