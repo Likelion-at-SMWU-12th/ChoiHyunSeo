7d22014e601f3ed29ae017f603a9cf18ce23bd07
kittyismylife <chss0520@sookmyung.ac.kr>
2024-06-25 19:05:13 +0900

    Feat: button style 실습 및 npm styled-components

--- FRONT/Front_Week3_Practice/practice/src/App.js
+++ FRONT/Front_Week3_Practice/practice/src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
+
 import Hello from "./Hello";
+import InlineButton from "./InlineButton";
+import ExternalButton from "./ExternalButton";
 
 function App() {
   return (
     <div>
       <h1>멋사 프론트 3주차 세미나</h1>
       <Hello />
+      <InlineButton />
+      <ExternalButton />
     </div>
   );
 }
