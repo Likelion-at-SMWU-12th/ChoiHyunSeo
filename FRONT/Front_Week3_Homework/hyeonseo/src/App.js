2224380b76c4497dbfafa402af3bef4b4616f325
kittyismylife <chss0520@sookmyung.ac.kr>
2024-06-28 18:02:17 +0900

    Feat: 메인화면 구성 수정

--- FRONT/Front_Week3_Homework/hyeonseo/src/App.js
+++ FRONT/Front_Week3_Homework/hyeonseo/src/App.js
@@ -1,11 +1,32 @@
-import "./App.css";
+import React, { useState } from "react";
 import Hello from "./Hello";
+import Secret from "./Secret";
+import Else from "./Else";
 
 function App() {
+  const [showOtherComponent, setShowOtherComponent] = useState(false);
+
+  const mainBoxStyle = {
+    height: "1000hv",
+    textAlign: "center",
+    padding: "50px",
+    backgroundColor: "#f0f0f0",
+    borderRadius: "10px",
+    boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+  };
+
+  const headingStyle = {
+    fontSize: "40px",
+    color: "black",
+    marginBottom: "20px",
+  };
+
   return (
-    <div id="mainbox">
-      <h1>🪐 Welcome to My Space 🪐</h1>
-      <Hello />
+    <div id="mainbox" style={mainBoxStyle}>
+      <h1 style={headingStyle}>🪐 Welcome to My Space 🪐</h1>
+      <Hello setShowOtherComponent={setShowOtherComponent} />
+      {showOtherComponent && <Secret />}
+      {showOtherComponent && <Else />}
     </div>
   );
 }
