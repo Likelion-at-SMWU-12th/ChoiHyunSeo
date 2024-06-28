bdee760ea32a1280b5f261b8d6b963b9bf7e7cbf
kittyismylife <chss0520@sookmyung.ac.kr>
2024-06-28 15:26:04 +0900

    Feat: 조건부 렌더링 복습

--- FRONT/Front_Week3_Homework/hyeonseo/src/App.js
+++ FRONT/Front_Week3_Homework/hyeonseo/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Hello from "./Hello";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>멋사 프론트 3주차</h1>
+      <Hello />
+    </div>
+  );
 }
 
 export default App;
---
+++ FRONT/Front_Week3_Homework/hyeonseo/src/Hello.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Hello = () => {
+  const name = "고양이지대조아";
+  if (name == "고양이지대조아") return <>조지냥! 야옹이의 페이지입니다!</>;
+  return <>고양이를 좋아하지 않는다면 출입할 수 없습니다.</>;
+};
+
+export default Hello;
