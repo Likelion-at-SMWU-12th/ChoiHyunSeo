c325ea10f4f4c095d418997b77e09aa6974b5384
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-01 14:38:13 +0900

    Feat: 프로필 추가

--- FRONT/Front_Week3_Homework/hyeonseo/src/MySite.jsx
+++ FRONT/Front_Week3_Homework/hyeonseo/src/MySite.jsx
@@ -1,22 +1,50 @@
 import React from "react";
 import styled from "styled-components";
+// import { Container } from "./MySite";
 
-const SiteBorder = styled.img`
-  padding: 6px 12px;
-  border: 1.8px solid black;
-  margin: 5px;
-  height: 80px;
-  width: 80px;
-  border-radius: 20px;
-  background-color: ${(props) => props.background || "#F6CECE"};
+const Container = styled.div`
+  padding: 40px 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 2px solid #045fb4;
+  border-radius: 40px;
+  margin-bottom: 40px;
+  background: #e1ecfe;
+`;
+
+const ProfilePicture = styled.img`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  margin-bottom: 5px;
+  border: 2px solid #879df7;
+`;
+
+const ProName = styled.p`
+  font-size: 23px;
+  background: white;
+  margin-bottom: 3px;
+  color: #045fb4;
+  border: 2px solid #045fb4;
+  padding: 8px 25px;
+  border-radius: 30px;
+`;
+
+const ProSub = styled.p`
+  font-size: 18px;
+  margin-bottom: 0px;
 `;
 
 const MySite = () => {
   return (
-    <div>
-      <button>d</button>
-    </div>
+    <Container>
+      <ProfilePicture src="img/profile.png" alt="Profile" />
+      <ProName>한해의 절반을 보내며...</ProName>
+      <ProSub>^^아 둥근해 미친거 또 떳네^^</ProSub>
+    </Container>
   );
 };
 
 export default MySite;
+export { Container };
