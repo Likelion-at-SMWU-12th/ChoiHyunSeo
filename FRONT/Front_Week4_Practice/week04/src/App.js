e19af4c2c31806007f84ea3cb284fd7e209798b2
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-02 18:37:03 +0900

    실습3: state 값의 변경
    상태 값이 변경될 때마다 카운터를 호출하여 리턴을 다시 받아 화면을 새롭게 그림!! -> 해당 과정을 리렌더라고 함

--- FRONT/Front_Week4_Practice/week04/src/App.js
+++ FRONT/Front_Week4_Practice/week04/src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
+import Counter from "./Counter";
 import Hello from "./Hello";
 
 function App() {
-  return <Hello name="고양이지대조아" color="blue" />;
+  return (
+    <>
+      {" "}
+      <Hello name="고양이지대조아" color="blue" />; <Counter />
+    </>
+  );
 }
 
 export default App;
