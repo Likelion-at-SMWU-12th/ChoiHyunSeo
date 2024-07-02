03c919b5f918c8b16a8540d7b22f5c51a9669035
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-03 02:47:49 +0900

    ㅎㅎ?

--- FRONT/Front_Week4_Practice/week04/src/App.js
+++ FRONT/Front_Week4_Practice/week04/src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import Counter from "./Counter";
 import Hello from "./Hello";
 import Info from "./Info";
+import MovieList from "./MovieList";
+
 function App() {
   return (
     <>
       <Info />
       <Hello name="고양이지대조아" color="blue" />
       <Counter />
+      <MovieList />
     </>
   );
 }
