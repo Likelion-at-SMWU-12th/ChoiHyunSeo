da88062d115105fdba396a8137f0ad4b0b635c60
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-08 15:12:09 +0900

    props를 활용

--- FRONT/Front_Week4_Homework/hyeonseo/src/App.js
+++ FRONT/Front_Week4_Homework/hyeonseo/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Hello from "./Hello";
 import Counter from "./Counter";
 import CatList from "./CatList";
 
 function App() {
   return (
     <>
+      <Counter name="고양이지대조아" />
       <CatList />
     </>
   );
--- FRONT/Front_Week4_Homework/hyeonseo/src/Counter.js
+++ FRONT/Front_Week4_Homework/hyeonseo/src/Counter.js
@@ -1,12 +1,21 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+import "./Counter.css";
 
-const Counter = () => {
+const Counter = (props) => {
   const [state, setState] = useState(0);
+
+  const handleDonate = () => {
+    setState(state + 100);
+  };
+
   return (
-    <div>
-      <h1>State 값: {state}</h1>
-      <button onClick={() => setState(state + 1)}>1씩 증가</button>
+    <div className="counter-container">
+      <h1>
+        {props.name}님의 사료 후원 금액: {state}원
+      </h1>
+      <button className="donate-button" onClick={handleDonate}>
+        100원 후원하기
+      </button>
     </div>
   );
 };
