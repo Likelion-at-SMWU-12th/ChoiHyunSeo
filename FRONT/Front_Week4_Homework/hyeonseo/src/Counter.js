47d78c83a7fa94b0fc6aa32a3baf6ec3bb63fb1a
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-08 11:55:38 +0900

    useState 복습

---
+++ FRONT/Front_Week4_Homework/hyeonseo/src/Counter.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+
+const Counter = () => {
+  const [state, setState] = useState(0);
+  return (
+    <div>
+      <h1>State 값: {state}</h1>
+      <button onClick={() => setState(state + 1)}>1씩 증가</button>
+    </div>
+  );
+};
+
+export default Counter;
