15ca82efe2cce231c9e2a5dec034954d9c33bd78
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-09 18:43:21 +0900

    Update App.js
    배열을 App에서 선언하고 UserList에게 props로 전달하기

--- FRONT/Front_Week5_Practice/hyeonseo/src/App.js
+++ FRONT/Front_Week5_Practice/hyeonseo/src/App.js
@@ -3,12 +3,18 @@ import InputSample from "./InputSample";
 import UserList from "./UserList";
 
 function App() {
+  const users = [
+    { id: 1, username: "sookmyung", email: "sookmyung@example.com" },
+    { id: 2, username: "mutsa", email: "likelionsmwu@example.com" },
+    { id: 3, username: "hyeonseo", email: "chss0520@sookmyung.com" },
+  ];
   return (
     <>
       <InputSample />;
       <br />
       <hr />
-      <UserList />
+      {/* <UserList /> */}
+      <UserList users={users} />
     </>
   );
 }
