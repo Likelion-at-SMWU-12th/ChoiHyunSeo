0fbcba529562ea35d673d18bf536314cf07dfb70
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-09 18:57:44 +0900

    useCallback
    배열에 새로운 항목을 추가할 수 있음! CreateUser 렌더링!

--- FRONT/Front_Week5_Practice/hyeonseo/src/App.js
+++ FRONT/Front_Week5_Practice/hyeonseo/src/App.js
@@ -1,25 +1,49 @@
 import "./App.css";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import InputSample from "./InputSample";
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({ username: "", email: "" });
+
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({ ...inputs, [name]: value });
+  };
+  const [users, setUsers] = useState([
     { id: 1, username: "sookmyung", email: "sookmyung@example.com" },
     { id: 2, username: "mutsa", email: "likelionsmwu@example.com" },
     { id: 3, username: "hyeonseo", email: "chss0520@sookmyung.com" },
-  ];
+  ]);
 
   const nextId = useRef(4);
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers(users.concat(user));
+
+    setInputs({
+      username: "",
+      email: "",
+    });
     nextId.current += 1;
   };
   return (
     <>
-      <InputSample />;
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
       <br />
       <hr />
-      {/* <UserList /> */}
       <UserList users={users} />
     </>
   );
