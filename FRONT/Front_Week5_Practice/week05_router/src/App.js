94bb5aef99a59f2a98fc7b1d52198f26d293ba31
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-09 20:10:52 +0900

    # 실습 종료

--- FRONT/Front_Week5_Practice/week05_router/src/App.js
+++ FRONT/Front_Week5_Practice/week05_router/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Introduce from "./pages/Introduce";
+import Menubar from "./pages/Menubar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Menubar />}>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/introduce" element={<Introduce />}></Route>
+      </Route>
+    </Routes>
   );
 }
 
