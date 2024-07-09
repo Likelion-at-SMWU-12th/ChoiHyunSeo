163b498200bf9ee49b165a1a35067c66dcbefb6f
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-09 18:45:22 +0900

    Update UserList.js
    App.js에서 전달해준 const userrk props로 전달

--- FRONT/Front_Week5_Practice/hyeonseo/src/UserList.js
+++ FRONT/Front_Week5_Practice/hyeonseo/src/UserList.js
@@ -9,18 +9,18 @@ const User = ({ user }) => {
   );
 };
 
-const UserList = () => {
-  const users = [
-    { id: 1, username: "sookmyung", email: "sookmyung@example.com" },
-    { id: 2, username: "mutsa", email: "likelionsmwu@example.com" },
-    { id: 3, username: "hyeonseo", email: "chss0520@sookmyung.com" },
-  ];
+const UserList = ({ users }) => {
+  //   const users = [
+  //     { id: 1, username: "sookmyung", email: "sookmyung@example.com" },
+  //     { id: 2, username: "mutsa", email: "likelionsmwu@example.com" },
+  //     { id: 3, username: "hyeonseo", email: "chss0520@sookmyung.com" },
+  //   ];
 
   return (
     <div>
-      <User user={users[0]} />
-      <User user={users[1]} />
-      <User user={users[2]} />
+      {users.map((user) => (
+        <User user={user} key={user.id} />
+      ))}
     </div>
   );
 };
