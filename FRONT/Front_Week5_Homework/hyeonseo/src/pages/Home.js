ab3fda8d012ee8d40c4599d445ebf83bfec67142
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-15 14:14:03 +0900

    페이지 추가 시도

---
+++ FRONT/Front_Week5_Homework/hyeonseo/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2 className="alt1">Welcome to Meow World!</h2>
+      <button className="buttonGo">Connecting to the Meow World</button>
+      <h1>내 집이다.</h1>
+      <p>내 야옹인 페이지다.</p>
+    </div>
+  );
+};
+
+export default Home;
