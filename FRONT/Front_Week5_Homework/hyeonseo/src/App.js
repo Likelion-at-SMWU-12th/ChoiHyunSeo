4442d0d5560bb46893adfea665e4e37f1be619bc
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-15 20:22:17 +0900

    POGI...

--- FRONT/Front_Week5_Homework/hyeonseo/src/App.js
+++ FRONT/Front_Week5_Homework/hyeonseo/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Early from "./pages/Early";
 import Home from "./pages/Home";
@@ -6,12 +6,6 @@ import User from "./pages/UserPage";
 import Menubar from "./pages/Menubar";
 
 function App() {
-  const [info, setInfo] = useState(null);
-
-  const handleInfoSubmit = (data) => {
-    setInfo(data);
-  };
-
   return (
     <>
       <Menubar />
