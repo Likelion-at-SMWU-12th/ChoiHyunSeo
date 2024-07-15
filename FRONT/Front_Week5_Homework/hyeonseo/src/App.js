9a3a607eaea565e39fe98bfb8b436f181fc3a2f0
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-15 20:14:03 +0900

    Update App.js

--- FRONT/Front_Week5_Homework/hyeonseo/src/App.js
+++ FRONT/Front_Week5_Homework/hyeonseo/src/App.js
@@ -1,19 +1,24 @@
-import "./App.css";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Early from "./pages/Early";
 import Home from "./pages/Home";
 import User from "./pages/UserPage";
-
 import Menubar from "./pages/Menubar";
 
 function App() {
+  const [info, setInfo] = useState(null);
+
+  const handleInfoSubmit = (data) => {
+    setInfo(data);
+  };
+
   return (
     <>
       <Menubar />
       <Routes>
         <Route path="/" element={<Early />} />
-        <Route path="home" element={<Home />} />
-        <Route path="user" element={<User />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/user" element={<User />} />
       </Routes>
     </>
   );
