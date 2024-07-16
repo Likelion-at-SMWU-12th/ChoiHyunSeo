1bf5f18fa1c3721761b4474855ce40879a4af7f2
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-16 20:02:37 +0900

    [Feat] CORS 해결, 특정 방명록 조회 등 구현

--- FRONT/Front_Week6_Practice/week06/src/components/Comment.jsx
+++ FRONT/Front_Week6_Practice/week06/src/components/Comment.jsx
@@ -1,60 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-let dummy = [
-  {
-    id: "1",
-    author: "강주은",
-    comment: "(예비) 프론트 최고 권위자",
-    timestamp: "2024-06-28T12:34:56Z",
-  },
-  {
-    id: "2",
-    author: "성윤정",
-    comment: "무럭무럭 쑥쑥 자라날개오",
-    timestamp: "2024-06-28T12:34:56Z",
-  },
-  {
-    id: "3",
-    author: "유동은",
-    comment: "멋쨍이 프론트 .. It’s ME",
-    timestamp: "2024-06-28T12:34:56Z",
-  },
-  {
-    id: "4",
-    author: "이현정",
-    comment: "Great!",
-    timestamp: "2024-06-28T12:34:56Z",
-  },
-  {
-    id: "5",
-    author: "장유빈",
-    comment: "움냠냠",
-    timestamp: "2024-06-28T12:34:56Z",
-  },
-  {
-    id: "6",
-    author: "최현서",
-    comment: "먛…",
-    timestamp: "2024-06-28T12:34:56Z",
-  },
-  {
-    id: "7",
-    author: "이예지",
-    comment:
-      "이렇게하고싶은말이너무많아서말이길어지면당연히텍스트영역이넓어지고보기에이쁘지가않겠죠그래서css로깔끔하게바꿔주려고합니다",
-    timestamp: "2024-06-28T12:34:56Z",
-  },
-];
-
 const Comment = () => {
   const navigate = useNavigate();
+  const [commnets, setCommnets] = useState([]);
+  const getCommnet = () => {
+    axios
+      // 메소드를 입력해주어야 함
+      .get("http://127.0.0.1:8000/entries/")
+      // 오류 없이 응답이 잘 도착했을 경우
+      .then((response) => {
+        console.log(response.data);
+        setCommnets(response.data.reverse());
+      })
+      // 에러가 발생했을 경우
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // 최초 렌더링 시, 한 번만 getComment 함수를 진행함
+  useEffect(() => {
+    getCommnet();
+  }, []);
+
   return (
     <>
-      {dummy.map((comment) => (
+      {/* 방명록 배열을 저장한 commnets를 map으로 보여주기 */}
+      {commnets.map((comment) => (
         // navigate를 사용하면 로컬에서 넘어가는 것
-        <CommentBox onClick={() => navigate(`/comment/${comment.id}`)}>
+        <CommentBox
+          key={comment.id}
+          onClick={() => navigate(`/comment/${comment.id}`)}
+        >
           <Author>{comment.author}</Author>
           <Time>{comment.timestamp}</Time>
           <CommentTxt>{comment.comment}</CommentTxt>
