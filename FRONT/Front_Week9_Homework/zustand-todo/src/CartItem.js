e0d267d12325bb4b1958a2b2e7f14557277883fc
kittyismylife <chss0520@sookmyung.ac.kr>
2024-09-23 13:58:05 +0900

    [Feat] UI 일부 수정

--- FRONT/Front_Week9_Homework/zustand-todo/src/CartItem.js
+++ FRONT/Front_Week9_Homework/zustand-todo/src/CartItem.js
@@ -7,11 +7,17 @@ function CartItem({ item }) {
 
   return (
     <li>
-      <span>물품명: {item.name}</span>
-      <span>가격: {item.price}원</span>
+      <p>
+        <span style={{ fontWeight: "bold" }}>물품명:&nbsp;</span>
+        <span>{item.name}&emsp;</span>
+      </p>
+      <p>
+        <span style={{ fontWeight: "bold" }}>가격:</span>
+        <span> {item.price}원&emsp;</span>
+      </p>
       <button onClick={() => decreaseQuantity(item.id)}>-</button>
-      <span>수량: {item.quantity}</span>
-      <button onClick={() => increaseQuantity(item.id)}>+</button>
+      <span>&nbsp;{item.quantity}&nbsp;</span>
+      <button onClick={() => increaseQuantity(item.id)}>+</button>&nbsp;
       <button onClick={() => removeProduct(item.id)}>삭제</button>
     </li>
   );
