fdc967e5d62b650118df0dbfc3b3590c1c0bbc1d
kittyismylife <chss0520@sookmyung.ac.kr>
2024-09-23 13:47:21 +0900

    [Fix] 쇼핑카트에 알맞게 수정하기

--- FRONT/Front_Week9_Homework/zustand-todo/src/store/store.js
+++ FRONT/Front_Week9_Homework/zustand-todo/src/store/store.js
@@ -1,31 +1,35 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 
-// 스토어 생성 (create를 통해 useTodoStore을 생성)
-const useTodoStore = create(
+const useCartStore = create(
   devtools((set) => ({
-    // 상태 초깃값 설정
-    todos: [],
-    // 액션 함수 작성
-    // todo 배열에 객체를 추가 (it, title, completed 속성 등)
-    addTodo: (title) =>
+    cartItems: [],
+    addProduct: (name, price) =>
       set((state) => ({
-        todos: [...state.todos, { id: Date.now(), title, completed: false }],
+        cartItems: [
+          ...state.cartItems,
+          { id: Date.now(), name, price, quantity: 1 },
+        ],
       })),
-    // id를 매개변수로 받아서 해당 id와 일치하지 않는 항목들만 남기고 필터링한 새로운 todos 배열을 만든다
-    // 해당 id 항목 삭제 효과
-    removeTodo: (id) =>
+    increaseQuantity: (id) =>
       set((state) => ({
-        todos: state.todos.filter((todo) => todo.id !== id),
-      })),
-    // map을 통해 todo 배열을 순회하면서 매개변수로 받은 id에 해당하는 객체를 찾아 completed 속성 변경
-    toggleTodo: (id) =>
-      set((state) => ({
-        todos: state.todos.map((todo) =>
-          todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        cartItems: state.cartItems.map((item) =>
+          item.id === id ? { ...item, quantity: item.quantity + 1 } : item
         ),
+      })),
+    decreaseQuantity: (id) =>
+      set((state) => ({
+        cartItems: state.cartItems.map((item) =>
+          item.id === id && item.quantity > 1
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        ),
+      })),
+    removeProduct: (id) =>
+      set((state) => ({
+        cartItems: state.cartItems.filter((item) => item.id !== id),
       })),
   }))
 );
 
-export default useTodoStore;
+export default useCartStore;
