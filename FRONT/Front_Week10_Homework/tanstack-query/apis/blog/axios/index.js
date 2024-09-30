f39271febdf4b6141a1cba8fac6410586866af0e
kittyismylife <chss0520@sookmyung.ac.kr>
2024-09-30 14:13:05 +0900

    [Fix] 실습 CRUD 게시판 만들어 보기 - 1

--- FRONT/Front_Week10_Homework/tanstack-query/apis/blog/axios/index.js
+++ FRONT/Front_Week10_Homework/tanstack-query/apis/blog/axios/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const createPost = async (newPost) => {
-  const { data } = await axios.post("/api/posts".newPost);
+  const { data } = await axios.post("/api/posts", newPost);
   return data;
 };
