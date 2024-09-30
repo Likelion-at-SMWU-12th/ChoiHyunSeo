5637a701d571238dbb7632ae67f8db87aadf9922
kittyismylife <chss0520@sookmyung.ac.kr>
2024-09-30 13:59:46 +0900

    [Feat] useQuery의 반환값 살펴보기

--- FRONT/Front_Week10_Homework/tanstack-query/src/App.js
+++ FRONT/Front_Week10_Homework/tanstack-query/src/App.js
@@ -1,6 +1,13 @@
 import "./App.css";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
+  async function getPosts() {
+    const response = await fetch(`https:jsonplaceholder.typicode.com/posts`);
+    return await response.json();
+  }
+  const queryResult = useQuery({ queryKey: ["post"], queryFn: getPosts });
+  console.log("queryResult", queryResult);
   return <div>tanstack query 실습하기</div>;
 }
 
