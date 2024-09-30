e40da9c4a26a0263511ac8f642be2f4dbd594629
kittyismylife <chss0520@sookmyung.ac.kr>
2024-09-30 18:20:26 +0900

    [Feat] 개인정보 수정 API

--- FRONT/Front_Week10_Homework/tanstack-query/apis/blog/axios/index.js
+++ FRONT/Front_Week10_Homework/tanstack-query/apis/blog/axios/index.js
@@ -1,6 +1,15 @@
 import axios from "axios";
 
+// 1. 회원가입
 export const signUp = async (userinfo) => {
+  // username, password를 받아 서버에 POST 요청을 보냄
   const { data } = await axios.post(`api/users/signup`, userinfo);
+  return data;
+};
+
+// 2. 개인정보 수정
+export const updateProfile = async (userId, updatedInfo) => {
+  // 새로운 사용자 정보를 받아 PUT 요청을 받음
+  const { data } = await axios.post(`api/users/profile/${userId}`, updatedInfo);
   return data;
 };
--- FRONT/Front_Week10_Homework/tanstack-query/apis/blog/queries/index.js
+++ FRONT/Front_Week10_Homework/tanstack-query/apis/blog/queries/index.js
@@ -5,12 +5,29 @@ import { updateProfile } from "../axios/index";
 import { mypageFetch } from "../axios/index";
 import { deleteUser } from "../axios/index";
 
+// 1. 회원가입
 export const useSignUp = (username, password) => {
+  // useMutation - 데이터 변이에 사용되는 hook
   return useMutation({
     mutationFn: ({ username, password }) => signUp(username, password),
     enabled: !!username && !!password,
     onSuccess: () => {
-      alert("환영합니다");
+      alert("환영합니다"); // 조건에 만족하면 메세지 출력
+    },
+  });
+};
+
+// 2. 개인정보 수정
+export const useUpdateProfile = (userId) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ userId, updatedInfo }) => updateProfile(userId, updatedInfo),
+    enabled: !!userId,
+    onSuccess: () => {
+      alert("개인 정보가 수정되었습니다");
+
+      // invalidateQueries를 사용하여 쿼리 다시 가져오기
+      queryClient.invalidateQueries({ queryKey: ["mypage"] });
     },
   });
 };
