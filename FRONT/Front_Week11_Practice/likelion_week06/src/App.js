5500bef3ddcebecf60cb520466589bb919c1022f
kittyismylife <chss0520@sookmyung.ac.kr>
2024-10-06 19:15:41 +0900

    [Fix] 코드 일부 수정

--- FRONT/Front_Week11_Practice/likelion_week06/src/App.js
+++ FRONT/Front_Week11_Practice/likelion_week06/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Redirection from "./Redirection";
+import Greeting from "./Greeting";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/oauth2" element={<Redirection />} />
+          <Route path="/greeting" element={<Greeting />} />
         </Routes>
       </BrowserRouter>
     </div>
--- FRONT/Front_Week11_Practice/likelion_week06/src/Login.js
+++ FRONT/Front_Week11_Practice/likelion_week06/src/Login.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import kakao_btn_img from "./kakao_login_large_narrow.png";
 import google_btn_img from "./web_light_sq_SI@2x.png";
 
 const Login = () => {
   const authServerLink = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`;
+
   const handleKakao = () => {
     window.location.href = authServerLink;
   };
@@ -15,10 +16,10 @@ const Login = () => {
         <button onClick={handleKakao}>
           <img src={kakao_btn_img} alt="kakao_button" />
         </button>
+        <button>
+          <img src={google_btn_img} alt="google_button" />
+        </button>
       </div>
-      <button>
-        <img src={google_btn_img} alt="goggle_button" />
-      </button>
     </div>
   );
 };
