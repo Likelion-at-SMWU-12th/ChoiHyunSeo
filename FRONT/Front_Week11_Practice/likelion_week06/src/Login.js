9035a9fc815822b40c43ff9f7f890e6c039c2aa6
kittyismylife <chss0520@sookmyung.ac.kr>
2024-10-06 19:58:50 +0900

    [Feat] 불필요한 부분 삭제하기

--- FRONT/Front_Week11_Practice/likelion_week06/src/Login.js
+++ FRONT/Front_Week11_Practice/likelion_week06/src/Login.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import kakao_btn_img from "./kakao_login_large_narrow.png";
 import google_btn_img from "./web_light_sq_SI@2x.png";
 
 const Login = () => {
   const authServerLink = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`;
+
   const handleKakao = () => {
     window.location.href = authServerLink;
   };
@@ -13,10 +14,10 @@ const Login = () => {
       <h2>로그인</h2>
       <div className="btns-container">
         <button onClick={handleKakao}>
-          <img src={kakao_btn_img} alt="kakao_btn" />
+          <img src={kakao_btn_img} alt="kakao_button" />
         </button>
         <button>
-          <img src={google_btn_img} alt="google_btn" />
+          <img src={google_btn_img} alt="google_button" />
         </button>
       </div>
     </div>
